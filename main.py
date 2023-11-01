363d8af0a5e2276a66323c49cc04ce5206222afb
Caleb <c.noss27@ncf.edu>
2023-11-01 11:31:37 -0400

    To use this application simply run the program then go to the website http://127.0.0.1:5000/MontyCarlo/(number of simulations you want to run in numbers such as 1, 2, or 3) The program will reply with the value of pi it calculated based on the number of simulations you asked for.

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import random
+app = Flask(__name__)
+
+@app.route('/MontyCarlo/<int:SimNum>')
+def MontyCarloSim(SimNum):
+    TotalSim = SimNum
+    SimInCircle = 0
+    for i in range(SimNum):
+        x = random.uniform(0,1)
+        y = random.uniform(0,1)
+        radius = x**2 + y**2
+        if radius <= 1:
+            SimInCircle += 1
+    return "<h1>Pi is roughly equal to </h1>" + str(4*(SimInCircle/TotalSim)) + "<h1> given the number of simulations you requested.</h1>"
+
+if __name__ == '__main__':
+    app.run()
+
+# To use this application simply run the program then go to the website
+# http://127.0.0.1:5000/MontyCarlo/(number of simulations you want to run in
+# numbers such as 1, 2, or 3) The program will reply with the value of pi it
+# calculated based on the number of simulations you asked for.
